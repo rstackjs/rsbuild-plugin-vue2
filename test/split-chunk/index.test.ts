60b6a51abaf8e62b744f653bb4025ce74d9d8434
neverland <chenjiahan.jait@bytedance.com>
2026-07-10 16:18:17 +0800

    chore: migrate tests to Rstest

--- test/split-chunk/index.test.ts
+++ test/split-chunk/index.test.ts
@@ -1,7 +1,7 @@
 import { existsSync } from 'node:fs';
 import path, { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { expect, test } from '@playwright/test';
+import { expect, test } from '@rstest/playwright';
 import { createRsbuild } from '@rsbuild/core';
 import { type PluginVueOptions, pluginVue2 } from '../../dist';
 
