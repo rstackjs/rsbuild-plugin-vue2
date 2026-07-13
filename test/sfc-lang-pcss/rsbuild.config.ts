f1fec3655fde35278f9350acd29718c6b39b6b7c
Jiahan Chen <chenjiahan.jait@bytedance.com>
2026-07-13 14:04:32 +0800

    chore: reuse shared test utilities (#71)

--- test/sfc-lang-pcss/rsbuild.config.ts
+++ test/sfc-lang-pcss/rsbuild.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginVue2 } from '@rsbuild/plugin-vue2';
-import { getRandomPort } from '../helper';
+import { getRandomPort } from '@rstackjs/test-utils';
 
 export default defineConfig({
   plugins: [pluginVue2()],
   server: {
-    port: getRandomPort(),
+    port: await getRandomPort(),
   },
 });
--- test/sfc-style/rsbuild.config.ts
+++ test/sfc-style/rsbuild.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginLess } from '@rsbuild/plugin-less';
 import { pluginVue2 } from '@rsbuild/plugin-vue2';
-import { getRandomPort } from '../helper';
+import { getRandomPort } from '@rstackjs/test-utils';
 
 export default defineConfig({
   plugins: [pluginVue2(), pluginLess()],
   server: {
-    port: getRandomPort(),
+    port: await getRandomPort(),
   },
 });
