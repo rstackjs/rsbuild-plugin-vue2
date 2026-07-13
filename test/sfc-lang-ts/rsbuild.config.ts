f3a02a5e0a9f04f0566b53054a87b4dcd06502a5
neverland <chenjiahan.jait@bytedance.com>
2026-07-13 13:56:29 +0800

    chore: remove test utility re-exports

--- test/sfc-lang-ts/rsbuild.config.ts
+++ test/sfc-lang-ts/rsbuild.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginVue2 } from '@rsbuild/plugin-vue2';
-import { getRandomPort } from '../helper';
+import { getRandomPort } from '@rstackjs/test-utils';
 
 export default defineConfig({
   plugins: [pluginVue2()],
--- test/sfc-style/rsbuild.config.ts
+++ test/sfc-style/rsbuild.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginLess } from '@rsbuild/plugin-less';
 import { pluginVue2 } from '@rsbuild/plugin-vue2';
-import { getRandomPort } from '../helper';
+import { getRandomPort } from '@rstackjs/test-utils';
 
 export default defineConfig({
   plugins: [pluginVue2(), pluginLess()],
